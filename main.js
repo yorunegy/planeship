3de98c0495b745db0519ba3d928b792e91645a18
yorunegy <yorunegy@gmail.com>
2019-09-04 13:07:34 +0800

    delete old JS file update script tab in HTML file

--- main.js
+++ main.js
@@ -19,67 +19,76 @@ let SHOOT_INTERVAL_FRAME = 5;
 // DisplaySceneという「画面全体のボス」から、子供のGameSceneという「ゲームプレイ画面」を作りました
 phina.define('MainScene', {
   superClass: 'DisplayScene',
-  init: function() { // ここはコンストラクタです。要は一番最初に一回だけやる処理のこと。
+  init: function(app) { // ここはコンストラクタです。要は一番最初に一回だけやる処理のこと。
     this.superInit(); // 親クラス（DisplayScene）のコンストラクタを呼んでます。
 
     this.backgroundColor = "#4682b4";
     // こんな感じで、飛行機オブジェクトを作って、画面に配置します。
     // WHITE
-    let white = Sprite("white", 48, 48).addChildTo(this);
-    white.x = this.gridX.center();
-    white.y = this.gridY.center();
-    this.white = white;  // MainSceneが持つwhiteという変数に、initが持つwhiteを入れた
+    this.white = White(this).addChildTo(this);
 
     // BLACK
-    let black = Sprite("black", 146, 96).addChildTo(this);
-    black.x = this.gridX.center();
-    black.y = this.gridY.span(4);
-    black.scaleY *= -1; // Y方向の"大きさ"を逆にすると見た目が逆になります。
-    this.speed = BLACK_SPEED;
-    this.black = black;
+    this.black = Black(app, this).addChildTo(this);
 
 
   },  // ここのカンマ忘れずに。MainSceneの中のinitとupdateです。
   // Scratchでいうと、「ずっと」です
   update:function(app) {
-      let white = this.white; // updateが持つwhiteに、MainSceneが持つwhiteを入れた
       const key = app.keyboard; // キーボードの状況が入った箱を持ってきます
-      if (key.getKey("right")) { // rightボタンが押されたらTrueになります
-        white.x += WHITE_SPEED;
-      }
-      if (key.getKey("left")) { // leftボタンが押されたらTrueになります
-        white.x += -WHITE_SPEED;
-      }
+      let white = this.white;
       if (key.getKey("space")) {
         Missile(white.x, white.y, this.black, app.frame).addChildTo(this);
       }
 
       let black = this.black;
-      if (black.x < 200 || black.x > 600) {
-          this.speed *= -1;
-      }
-      black.x += this.speed;
-
       EnemyMissile(black.x, black.y, this.white).addChildTo(this);
   },
 });
 
+phina.define("White", {
+    superClass:"Sprite",
+    init:function(scene) {
+        this.superInit("white", 48, 48);
+        this.x = scene.gridX.center();
+        this.y = scene.gridY.center();
+    },
+    update:function(app) {
+        const key = app.keyboard;
+        if (key.getKey("right")) { // rightボタンが押されたらTrueになります
+            this.x += WHITE_SPEED;
+        }
+        if (key.getKey("left")) { // leftボタンが押されたらTrueになります
+            this.x += -WHITE_SPEED;
+        }
+    }
+});
+
+phina.define("Black", {
+    superClass:"Sprite",
+    init:function(app, scene) {
+        this.superInit("black", 146, 96);
+        this.x = scene.gridX.center();
+        this.y = scene.gridY.span(4);
+        this.scaleY *= -1; // Y方向の"大きさ"を逆にすると見た目が逆になります。
+        this.speed = BLACK_SPEED;
+    },
+    update:function() {
+        if (this.x < 200 || this.x > 600) {
+            this.speed *= -1;
+        }
+        this.x += this.speed;
+    }
+});
+
 phina.define("Missile", {
   superClass:"Sprite",
-  shootCount:0,
   init:function(x, y, black, frame) {
-    if (this.shootCount > MAX_SHOOT_COUNT) {
-      this.shootCount = 0;
-      return;
-    } else {
       this.superInit("missile", 63, 19);
-      this.shootCount += 1;
       this.x = x;
       this.y = y - 20;
       this.setRotation(90);
       this.physical.velocity.y = -5;
       this.black = black;
-    }
   },
   update:function() {
     if (this.top < 0) {
