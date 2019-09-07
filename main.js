b34b2830fea057749bd2a9e5fcbad60f9897d039
yorunegy <yorunegy@gmail.com>
2019-09-08 00:58:04 +0800

    黒い飛行機がremove後に、その場に見えないが残り続ける問題を解消 DisplayElementの利用 アロー関数によるforEachの利用

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ let ASSETS = {
     image:{
         white:"img/planeship/white.png",
         black:"img/planeship/black.png",
-        missile:"img/planeship/missile.png"
+        missile:"img/planeship/missile.png",
+        bg:"img/planeship/sky.jpg"
     }
 };
 
@@ -13,6 +14,15 @@ let WHITE_SPEED = 5;
 let BLACK_SPEED = 3;
 let MAX_SHOOT_COUNT = 3;
 let SHOOT_INTERVAL_FRAME = 5;
+let BLACK_HP = 1;
+let WHITE_HP = 50;
+
+let enemy;
+let player;
+let playerMissileGroup;
+let enemyMissileGroup;
+let whiteHpLabel;
+let blackHpLabel; 
 // phinaではこうやってClassを定義します
 // Classよくわからんって人は、「ここで画面（Scene）を作ってる」とおぼえましょう
 // ゲーム業界では画面のことをSceneと呼びます。スタート画面、ゲームプレイの画面、メニュー画面など
@@ -22,27 +32,59 @@ phina.define('MainScene', {
   init: function(app) { // ここはコンストラクタです。要は一番最初に一回だけやる処理のこと。
     this.superInit(); // 親クラス（DisplayScene）のコンストラクタを呼んでます。
 
-    this.backgroundColor = "#4682b4";
+    this.backgroundColor = "#4169e1";
+
+    playerMissileGroup = DisplayElement().addChildTo(this);
+    enemyMissileGroup = DisplayElement().addChildTo(this);
+    this.playergroup = DisplayElement().addChildTo(this);
+    this.enemygroup = DisplayElement().addChildTo(this);
     // こんな感じで、飛行機オブジェクトを作って、画面に配置します。
     // WHITE
-    this.white = White(this).addChildTo(this);
-
+    player = White(this).addChildTo(this.playergroup);
     // BLACK
-    this.black = Black(app, this).addChildTo(this);
+    enemy = Black(this).addChildTo(this.enemygroup);
 
+    // BLACKとWHITEのHPを設定
+    this.whiteHp = WHITE_HP;
+    this.blackHp = BLACK_HP;
 
+    // BLACKとWHITEのHPを表示
+    this.whiteHpLabel = hpLabel("white", this.whiteHp, 330, 600).addChildTo(this);
+    this.blackHpLabel = hpLabel("black", this.blackHp, 330, 100).addChildTo(this);
   },  // ここのカンマ忘れずに。MainSceneの中のinitとupdateです。
   // Scratchでいうと、「ずっと」です
   update:function(app) {
-      const key = app.keyboard; // キーボードの状況が入った箱を持ってきます
-      let white = this.white;
-      if (key.getKey("space")) {
-        Missile(white.x, white.y, this.black, app.frame).addChildTo(this);
-      }
-
-      let black = this.black;
-      EnemyMissile(black.x, black.y, this.white).addChildTo(this);
+    const key = app.keyboard;
+    if (key.getKey("space")) {
+      Missile(player.x, player.y,this.blackHpLabel).addChildTo(playerMissileGroup);
+    }
+    this.hitTestMissileToPlayer();
   },
+  hitTestMissileToPlayer:function() {
+    playerMissileGroup.children.forEach(missile => {
+      this.enemygroup.children.forEach(enemy => {
+        if (missile.hitTestElement(enemy)) {
+          this.blackHp -= 1;
+          missile.remove();
+          if (this.blackHp === 0) {
+            enemy.remove();
+          }
+        }
+      })
+    })
+  }
+});
+
+phina.define("BackGround", {
+  superClass:"DisplayElement",
+  init:function() {
+    this.superInit();
+    let bg = Sprite("bg", 1280, 1920).addChildTo(this);
+
+  },
+  update:function() {
+
+  }
 });
 
 phina.define("White", {
@@ -51,6 +93,7 @@ phina.define("White", {
         this.superInit("white", 48, 48);
         this.x = scene.gridX.center();
         this.y = scene.gridY.center();
+        this.blackHpLabel = scene.blackHpLabel;
     },
     update:function(app) {
         const key = app.keyboard;
@@ -65,12 +108,21 @@ phina.define("White", {
 
 phina.define("Black", {
     superClass:"Sprite",
-    init:function(app, scene) {
+    init:function(scene) {
         this.superInit("black", 146, 96);
         this.x = scene.gridX.center();
         this.y = scene.gridY.span(4);
         this.scaleY *= -1; // Y方向の"大きさ"を逆にすると見た目が逆になります。
         this.speed = BLACK_SPEED;
+        this.tweener.clear()
+                    .call(function() {
+                      this.shot();
+                    }, this)
+                    .wait(600)
+                    .setLoop(true);
+    },
+    shot:function() {
+        EnemyMissile(this.x, this.y).addChildTo(this);
     },
     update:function() {
         if (this.x < 200 || this.x > 600) {
@@ -82,44 +134,68 @@ phina.define("Black", {
 
 phina.define("Missile", {
   superClass:"Sprite",
-  init:function(x, y, black, frame) {
+  init:function(x, y, hpLabel) {
       this.superInit("missile", 63, 19);
       this.x = x;
       this.y = y - 20;
       this.setRotation(90);
       this.physical.velocity.y = -5;
-      this.black = black;
+      this.hpLabel = hpLabel;
   },
   update:function() {
     if (this.top < 0) {
       this.remove();
     }
-    if (this.hitTestElement(this.black)) {
+    /*
+    if (this.hitTestElement(enemy)) {
+      this.hpLabel.hp -= 1;
+      this.hpLabel.text = "black:" + this.hpLabel.hp;
+      if (this.hpLabel.hp === 0) {
+        enemy.remove();
+      }
       this.remove();
     }
+    */
   }
 });
 
 phina.define("EnemyMissile", {
   superClass:"Sprite",
-  init:function(x, y, white) {
+  init:function(x, y) {
     this.superInit("missile", 63, 19);
     this.x = x;
     this.y = y + 20;
     this.setRotation(-90);
     this.physical.velocity.y = 5;
-    this.white = white;
+
   },
   update:function() {
     if (this.top > 900) {
       this.remove();
     }
-    if (this.hitTestElement(this.white)) {
+    if (this.hitTestElement(player)) {
       this.remove();
     }
   }
-})
+});
 
+
+phina.define("hpLabel", {
+  superClass:"Label",
+  init:function(name, hp, x, y) {
+      this.superInit(name + ":" + hp, 50,50);
+      this.x = x;
+      this.y = y;
+      this.hp = hp;
+      this.name = name;
+  },
+  decreaseHp:function(damage) {
+      this.hp -= damage;
+  },
+  update:function() {
+    this.text = this.name + ":" + this.hp;
+  }
+});
 
 
 // ここがゲームスタートの入り口です
